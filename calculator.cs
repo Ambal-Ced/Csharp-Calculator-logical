89c7b1de4de1b0bb31d13dcb5446e3b48dba6607
Ambal_Ced <147784891+Ambal-Ced@users.noreply.github.com>
2024-08-20 07:43:01 +0000

    create function

---
+++ calculator.cs
@@ -0,0 +1,12 @@
+bool isingui = true;
+process(isingui);
+
+void process(bool isgui){
+	
+}
+void verifyInput(){}
+void multiply(){}
+void division(){}
+void addition(){}
+void subtraction(){}
+void exit(){}
